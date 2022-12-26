3d9b2f83acf0df8d84f55441f149419c5ed85f51
romijatmiko <romijatmiko033@gmail.com>
2022-12-26 17:25:19 +0700

    update

--- src/pages/user/login.jsx
+++ src/pages/user/login.jsx
@@ -20,8 +20,8 @@ const LoginJura = () => {
 
 	useEffect(() => {
 		if (user || isSuccess) {
-			// navigate("/");
-			// window.location.reload();
+			navigate("/");
+			window.location.reload();
 		}
 	}, [user, isSuccess, dispatch, navigate, location]);
 
